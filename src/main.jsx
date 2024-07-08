e5725ed0320cfd6674645b960f08d8c1c49ce783
changlongwu <changlong_wu@berkeley.edu>
2024-07-07 21:28:54 -0700

    x

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
   <BrowserRouter>
   <Routes>
-    <Route path='/crypto_api' element={<Layout/>}>
+    <Route path='/crypto_api/' element={<Layout/>}>
       <Route index={true}  element={<App />} />
       <Route index={false} path="/coinDetails/:symbol" element={<DetailView/>}   />
       
